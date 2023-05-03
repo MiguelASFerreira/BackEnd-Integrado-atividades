bd3e102e1d74106d8a115f69466eec9aa7edc35a
Miguel <migant1306@gmail.com>
2023-05-03 19:04:43 -0300

    calculadora

---
+++ calculadora/calc.js
@@ -0,0 +1,20 @@
+function soma(n1, n2) {
+    console.log("Soma:", n1 + n2);
+}
+
+function subtrair(n1, n2) {
+    console.log("Subtração:", n1 - n2);
+}
+
+function dividir(n1, n2) {
+    console.log("Divisão:", n1 / n2);
+}
+
+function multi(n1, n2) {
+    console.log("Multiplicação:", n1 * n2);
+}
+
+soma(5,5);
+subtrair(10,5);
+dividir(5,5);
+multi(5,5);
