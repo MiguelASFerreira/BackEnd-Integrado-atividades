05858a6cebc509e169c73b13f071d4829a9794f0
Miguel <migant1306@gmail.com>
2023-05-03 19:13:34 -0300

    impar e par

--- calculadora/calc.js
+++ calculadora/calc.js
@@ -1,17 +1,40 @@
+var num1;
+var num2;
+
 function soma(n1, n2) {
-    console.log("Soma:", n1 + n2);
+    var soma = n1 + n2;
+    if (soma%2 == 0) {
+        console.log("Soma:", soma, ". É par");
+    } else {
+        console.log("Soma:", soma, ". É ímpar");
+    }
 }
 
 function subtrair(n1, n2) {
-    console.log("Subtração:", n1 - n2);
+    var sub = n1 - n2;
+    if (sub%2 == 0) {
+        console.log("Subtração:", sub, ". É par");
+    } else {
+        console.log("Subtração:", sub, ". É ímpar");
+    }
 }
 
 function dividir(n1, n2) {
-    console.log("Divisão:", n1 / n2);
+    var div = n1 / n2;
+    if (div%2 == 0) {
+        console.log("Divisão:", div, ". É par");
+    } else {
+        console.log("Divisão:", div, ". É ímpar");
+    }
 }
 
 function multi(n1, n2) {
-    console.log("Multiplicação:", n1 * n2);
+    var multi = n1 / n2;
+    if (multi%2 == 0) {
+        console.log("Multiplicação:", multi, ". É par");
+    } else {
+        console.log("Multiplicação:", multi, ". É ímpar");
+    }
 }
 
 soma(5,5);
